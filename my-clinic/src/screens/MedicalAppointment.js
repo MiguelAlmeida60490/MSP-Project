29745b03fd2e5a363f7435222b7da835295572cd
JoãoPereira60974 <jpb.pereira@campus.fct.unl.pt>
2024-04-16 21:11:25 +0100

    Datepicker now has time param

--- my-clinic/src/screens/MedicalAppointment.js
+++ my-clinic/src/screens/MedicalAppointment.js
@@ -4,12 +4,16 @@ import { styles } from "../styles/styles";
 import { useNavigation } from "@react-navigation/native";
 import { useAuth } from "../contexts/authContext";
 import DatePicker from 'react-native-modern-datepicker';
+import { TextInput } from "react-native-gesture-handler";
+
 
 const MedicalAppointment = () => {
   const navigation = useNavigation();
   const { currentUser, userData } = useAuth();
-  const [date, setDate] = useState();
+  const [date, setDate] = useState('');
   const [open, setOpen] = useState(false);
+  const [doctor, setDoctor] = useState("");
+
 
   function handleOnPress() {
     setOpen(!open);
@@ -21,15 +25,13 @@ const MedicalAppointment = () => {
       {date ? (<Text>Date Picked: {date}</Text>) : (<Text>No date selected</Text>)}
       <View style={styles.buttonView}>
         <TouchableOpacity onPress={handleOnPress}>
-          <Text>Open</Text>
+          <Text>Choose Date</Text>
         </TouchableOpacity>
         <Modal animationType="slide" transparent={true} visible={open}>
           <View style={styles.centeredView}>
             <View style={styles.modalView}>
               <DatePicker
-                mode='calendar'
-                selected={date}
-                onDateChange={(date) => setDate(date)}
+                onSelectedChange={date => setDate(date)}
               />
               <TouchableOpacity onPress={handleOnPress}>
                 <Text>Close</Text>
@@ -37,6 +39,12 @@ const MedicalAppointment = () => {
             </View>
           </View>
         </Modal>
+        <TextInput
+        style={styles.input}
+        placeholder="Preferred Doctor"
+        value={doctor}
+        onChangeText={(doctor) => setDoctor(doctor)}
+      ></TextInput>
       </View>
     </View>
   );
