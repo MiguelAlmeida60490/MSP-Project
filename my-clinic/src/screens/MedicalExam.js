bc17150b1035fa19b0ec2278d9ff86495c0759f8
Miguel Almeida <mv.almeida@campus.fct.unl.pt>
2024-04-20 12:36:45 +0100

    Added medicalExam

--- my-clinic/src/screens/MedicalExam.js
+++ my-clinic/src/screens/MedicalExam.js
@@ -6,20 +6,134 @@ import { useAuth } from "../contexts/authContext";
 import DatePicker from "react-native-modern-datepicker";
 import { TextInput } from "react-native-gesture-handler";
 import { app } from "../services/firebaseConfig";
+import DropDownPicker from "react-native-dropdown-picker";
 
 const MedicalExam = () => {
-  const [date, setDate] = useState("");
-  const [open, setOpen] = useState(false);
-  const [doctor, setDoctor] = useState("");
-  const [desc, setDesc] = useState("");
   const [loading, setLoading] = useState("");
+
+  const [date, setDate] = useState("");
+  const [openDate, setOpenDate] = useState(false);
+
+  const [listDoctors, setListDoctors] = useState([]);
+  const [openDoctors, setOpenDoctors] = useState(false);
+  const [doctorValue, setDoctorValue] = useState(null);
+
+  const [listEquipments, setListEquipments] = useState([
+    { label: "X-Ray", value: "x-ray" },
+    { label: "Tac", value: "tac" },
+    { label: "Magnetic Resonance", value: "magnetic resonance" },
+    { label: "Ultrasound", value: "ultrasound" },
+  ]);
+  const [openEquipments, setOpenEquipments] = useState(false);
+  const [equipmentValue, setEquipmentValue] = useState(null);
+
+  const [desc, setDesc] = useState("");
 
   const navigation = useNavigation();
   const { currentUser, userData } = useAuth();
 
+  useEffect(() => {
+    app
+      .firestore()
+      .collection("users")
+      .onSnapshot((querySnapshot) => {
+        const newDoctors = [];
+        querySnapshot.forEach((doc) => {
+          const { email, name, role } = doc.data();
+          if (doc.data().role === "doctor") {
+            newDoctors.push({ label: "Dr. " + name, value: email });
+          }
+        });
+        setListDoctors(newDoctors);
+      });
+  }, []);
+
+  const confirmMedicalExam = async () => {
+    setLoading(true);
+    try {
+      handleAddExam();
+      setLoading(false);
+      navigation.navigate("Home");
+    } catch (error) {
+      setLoading(false);
+      alert("Register error: " + error.message);
+    }
+  };
+
+  const handleAddExam = () => {
+    app.firestore().collection("examEquipment").add({
+      checkIn: false,
+      client: userData.email,
+      date,
+      doctor: doctorValue,
+      equipment: equipmentValue,
+      desc,
+    });
+  };
+
+  function handleOnPress() {
+    setOpenDate(!openDate);
+  }
+
   return (
-    <View>
-      <Text>MedicalExam</Text>
+    <View style={styles.container}>
+      <Text style={styles.mainText}>Schedule your appointment</Text>
+      {date ? <Text>Date Picked: {date}</Text> : <Text>No date selected</Text>}
+      <View>
+        <TouchableOpacity style={styles.dateButton} onPress={handleOnPress}>
+          <Text style={styles.centerText}>
+            {date ? "Change Date" : "Choose Date"}
+          </Text>
+        </TouchableOpacity>
+        <Modal animationType="slide" transparent={true} visible={openDate}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <DatePicker onSelectedChange={(date) => setDate(date)} />
+              <TouchableOpacity onPress={handleOnPress}>
+                <Text>Close</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+      </View>
+      <DropDownPicker
+        open={openDoctors}
+        value={doctorValue}
+        items={listDoctors}
+        setOpen={setOpenDoctors}
+        setValue={setDoctorValue}
+        setItems={setListDoctors}
+        style={styles.dropdown}
+        placeholder="Select your preferred doctor"
+        textStyle={{ fontSize: 18, fontWeight: "bold" }}
+      />
+      <DropDownPicker
+        open={openEquipments}
+        value={equipmentValue}
+        items={listEquipments}
+        setOpen={setOpenEquipments}
+        setValue={setEquipmentValue}
+        setItems={setListEquipments}
+        style={styles.dropdown}
+        placeholder="Select the type of exam"
+        textStyle={{ fontSize: 18, fontWeight: "bold" }}
+      />
+      <TextInput
+        style={styles.textBox}
+        placeholder="Description"
+        value={desc}
+        onChangeText={(desc) => setDesc(desc)}
+        multiline={true}
+        textAlignVertical="top"
+      ></TextInput>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          confirmMedicalExam();
+        }}
+      >
+        <Text style={styles.buttonText}>Confirm Medical Exam</Text>
+      </TouchableOpacity>
     </View>
   );
 };
